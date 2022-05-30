42390eeed87168a63bae06a91ab7da5bb7c72f84
Morgana <morganabagatini@gmail.com>
2022-05-30 09:46:14 -0300

    Categories - swagger doc

--- 02-locadora-cars/src/index.ts
+++ 02-locadora-cars/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import swaggerUi from 'swagger-ui-express';
 
 import { router } from "./routes";
-import swaggerConfig from "./swagger.json";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(router);
 
-app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3333, () => {
 	console.log("🌐 Server is running on port 3333");
