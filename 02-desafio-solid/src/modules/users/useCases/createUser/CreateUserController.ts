f0305db055c1581083cdd2f11aab6380cc1c4fae
Morgana <morganabagatini@gmail.com>
2022-05-30 10:55:37 -0300

    Create user

---
+++ 02-desafio-solid/src/modules/users/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+	constructor(private createUserUseCase: CreateUserUseCase) { }
+
+	handle(request: Request, response: Response): Response {
+		const { name, email } = request.body;
+
+		try {
+			return response.json(this.createUserUseCase.execute({ name, email }));
+		} catch (error) {
+			return response.status(400).json({ error: error.message });
+		}
+	}
+}
+
+export { CreateUserController };
