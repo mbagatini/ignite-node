855f1c81727d5dec80705d04546cc02fee25ae70
Morgana <morganabagatini@gmail.com>
2023-03-28 16:21:05 -0300

    refact: move routes folder

--- 03-api-solid/src/http/routes/index.ts
+++ 03-api-solid/src/http/routes/index.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
+import { sessionRoutes } from './sessions.routes'
 import { userRoutes } from './users.routes'
 
 export async function appRoutes(app: FastifyInstance) {
 	app.register(userRoutes, {
 		prefix: '/users',
+	})
+	app.register(sessionRoutes, {
+		prefix: '/sessions',
 	})
 }
