1e250c4f19dae0b29b23c0a2a8ebaa773cb61b7f
Morgana <morganabagatini@gmail.com>
2023-05-22 22:06:50 -0300

    feat: check-in dtos

---
+++ 03-api-solid/src/dto/check-in.ts
@@ -0,0 +1,12 @@
+export type CheckIn = {
+	id: string
+	user_id: string
+	gym_id: string
+	created_at: Date
+	updated_at?: Date | null
+}
+
+export type CheckInCreation = {
+	user_id: string
+	gym_id: string
+}
