17feb82b016bfdaaa3deec14273ab4834b2650fd
Morgana <morganabagatini@gmail.com>
2023-05-23 21:51:20 -0300

    feat: checkin use case distance validation

--- 03-api-solid/src/use-cases/check-in.ts
+++ 03-api-solid/src/use-cases/check-in.ts
@@ -1,9 +1,14 @@
 import { CheckIn } from '@/dto/check-in'
+import { ResourceNotFoundError } from '@/errors/resource-not-found-error'
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 interface CheckInUseCaseRequest {
 	userId: string
 	gymId: string
+	userLatitude: number
+	userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -11,18 +16,46 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-	constructor(private checkInsRepository: CheckInsRepository) {}
+	constructor(
+		private checkInsRepository: CheckInsRepository,
+		private gymsRepository: GymsRepository,
+	) {}
 
 	async execute({
 		userId,
 		gymId,
+		userLatitude,
+		userLongitude,
 	}: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
-		const checkInAlreadyDone = await this.checkInsRepository.findByUserIdOnDate(
+		const gym = await this.gymsRepository.findById(gymId)
+
+		if (!gym) {
+			throw new ResourceNotFoundError('Gym not found')
+		}
+
+		const distance = getDistanceBetweenCoordinates(
+			{
+				latitude: userLatitude,
+				longitude: userLongitude,
+			},
+			{
+				latitude: gym.latitude,
+				longitude: gym.longitude,
+			},
+		)
+
+		const MAX_DISTANCE_IN_KM = 0.1
+
+		if (distance > MAX_DISTANCE_IN_KM) {
+			throw new Error('You are too far away from the gym to check in')
+		}
+
+		const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
 			userId,
 			new Date(),
 		)
 
-		if (checkInAlreadyDone) {
+		if (checkInOnSameDate) {
 			throw new Error('Check-in already done today')
 		}
 
