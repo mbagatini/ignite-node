07606c46fb64ca4d691a69fa3d6515cf26551790
Morgana <morganabagatini@gmail.com>
2023-05-23 21:52:59 -0300

    refactor: checkin factory updated dependencies

--- 03-api-solid/src/use-cases/factories/make-check-in-use-case.ts
+++ 03-api-solid/src/use-cases/factories/make-check-in-use-case.ts
@@ -1,9 +1,11 @@
-import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 import { CheckInUseCase } from '../check-in'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 
 export function makeCheckInUseCase(): CheckInUseCase {
 	const checkInsRepository = new PrismaCheckInsRepository()
-	const checkInUseCase = new CheckInUseCase(checkInsRepository)
+	const gymsRepository = new PrismaGymsRepository()
+	const checkInUseCase = new CheckInUseCase(checkInsRepository, gymsRepository)
 
 	return checkInUseCase
 }
