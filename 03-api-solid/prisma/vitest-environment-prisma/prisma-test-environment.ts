b5178c6480d5ea6a8d33e260f400b6d8e4908ce0
Morgana <morganabagatini@gmail.com>
2023-11-06 22:52:35 -0300

    tests: prepare e2e test environment

---
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,41 @@
+import { prisma } from '@/database/prisma'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import { Environment } from 'vitest'
+
+function generateDatabaseURL(schema: string) {
+	if (!process.env.DATABASE_URL) {
+		throw Error('Please provide the DATABASE_URL environment variable')
+	}
+
+	const url = new URL(process.env.DATABASE_URL)
+
+	url.searchParams.set('schema', schema)
+
+	return url.toString()
+}
+
+export default <Environment>{
+	name: 'prisma',
+	// before running the tests
+	async setup() {
+		const uuid = randomUUID()
+		const databaseUrl = generateDatabaseURL(uuid)
+
+		// creates a new schema to run e2e tests
+		process.env.DATABASE_URL = databaseUrl
+
+		execSync('npx run prisma migrate deploy')
+
+		// after all tests
+		return {
+			async teardown() {
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${uuid}" CASCADE`,
+				)
+
+				await prisma.$disconnect()
+			},
+		}
+	},
+}
