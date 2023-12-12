88aa0e8d8393eafba10aefc257de01933593a074
Morgana <morganabagatini@gmail.com>
2023-12-11 22:11:51 -0300

    feat: verify admin routes

--- 03-api-solid/src/http/routes/check-ins.routes.ts
+++ 03-api-solid/src/http/routes/check-ins.routes.ts
@@ -4,13 +4,18 @@ import { validate } from '../controllers/check-ins/validate'
 import { metrics } from '../controllers/check-ins/metrics'
 import { history } from '../controllers/check-ins/history'
 import { create } from '../controllers/check-ins/create'
+import { verifyUserRole } from '../middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
 	/** must be authenticated */
 	app.addHook('onRequest', verifyJwt)
 
 	app.post('/gyms/:gymId/check-in', create)
-	app.patch('/check-ins/:checkInId/validate', validate)
+	app.patch(
+		'/check-ins/:checkInId/validate',
+		{ onRequest: [verifyUserRole('ADMIN')] },
+		validate,
+	)
 	app.get('/check-ins/metrics', metrics)
 	app.get('/check-ins/history', history)
 }
--- 03-api-solid/src/http/routes/gyms.routes.ts
+++ 03-api-solid/src/http/routes/gyms.routes.ts
@@ -3,12 +3,13 @@ import { verifyJwt } from '../middlewares/verify-jwt'
 import { create } from '../controllers/gyms/create'
 import { search } from '../controllers/gyms/search'
 import { nearby } from '../controllers/gyms/nearby'
+import { verifyUserRole } from '../middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
 	/** must be authenticated */
 	app.addHook('onRequest', verifyJwt)
 
-	app.post('/', create)
+	app.post('/', { onRequest: [verifyUserRole('ADMIN')] }, create)
 	app.get('/search', search)
 	app.get('/nearby', nearby)
 }
