6348beccbad42724e216580228745d2d626a208d
Morgana <morganabagatini@gmail.com>
2024-01-08 23:10:31 -0300

    chore: first commit

---
+++ challenge-ignite-nodejs-03-find-a-friend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+		"standard-with-typescript", 'plugin:prettier/recommended'
+	],
+	plugins: [
+		"prettier"
+	],
+    overrides: [
+    ],
+    parserOptions: {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 80,
+				tabWidth: 4,
+				singleQuote: true,
+				trailingComma: 'all',
+				arrowParens: 'always',
+				semi: false,
+			},
+		],
+    }
+}
