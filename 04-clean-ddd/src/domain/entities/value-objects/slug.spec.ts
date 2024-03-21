6241a6e18f3501f22e9e09b494756f15ce2c6f62
Morgana <morganabagatini@gmail.com>
2024-03-21 07:55:55 -0300

    feat: slug class

---
+++ 04-clean-ddd/src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test('should be able to create a new slug from a text', () => {
+	const slug = Slug.createFromText('Some title with spaces')
+	expect(slug.value).toBe('some-title-with-spaces')
+})
