0949a711d4c27b5457391e08fef852063cbac9ed
Morgana <morganabagatini@gmail.com>
2024-05-16 21:32:04 -0300

    test: fake data factory

--- 04-clean-ddd/src/test/factories/make-question.ts
+++ 04-clean-ddd/src/test/factories/make-question.ts
@@ -1,18 +1,23 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { faker } from '@faker-js/faker'
 import {
     Question,
     type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps>): Question {
-    const question = Question.create({
-        authorId: new UniqueEntityID(),
-        slug: Slug.create('example-question'),
-        title: 'Nova pergunta',
-        content: 'Conteúdo da pergunta',
-        ...override,
-    })
+export function makeQuestion(
+    override: Partial<QuestionProps> = {},
+    id?: UniqueEntityID,
+): Question {
+    const question = Question.create(
+        {
+            authorId: new UniqueEntityID(),
+            title: faker.lorem.sentence(),
+            content: faker.lorem.text(),
+            ...override,
+        },
+        id,
+    )
 
     return question
 }
