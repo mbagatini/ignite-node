a965976d48092631ddf20b7d9d9e0cd41f248763
Morgana <morganabagatini@gmail.com>
2024-07-14 19:36:39 -0300

    feat: create question use case

--- 05-api-nest/src/controllers/create-question.controller.ts
+++ 05-api-nest/src/controllers/create-question.controller.ts
@@ -1,40 +1,47 @@
+import { AuthenticatedUser } from '@/auth/authenticated-user.decorator';
+import { JwtAuthGuard } from '@/auth/jwt-auth-guard';
+import { UserPayload } from '@/auth/jwt.strategy';
 import { ZodValidationPipe } from '@/pipes/zod-validation-pipe';
 import { PrismaService } from '@/prisma/prisma.service';
-import {
-  Body,
-  Controller,
-  HttpCode,
-  Post,
-  UseGuards,
-  UsePipes,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { z } from 'zod';
 
-const CreateQuestionSchema = z.object({
+const createQuestionSchema = z.object({
   title: z.string(),
-  description: z.string(),
+  content: z.string(),
 });
 
-type CreateQuestionBodySchema = z.infer<typeof CreateQuestionSchema>;
+type CreateQuestionBodySchema = z.infer<typeof createQuestionSchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionSchema);
 
 @Controller('/questions')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  @UsePipes(new ZodValidationPipe(CreateQuestionSchema))
-  async handle(@Body() body: CreateQuestionBodySchema) {
-    return { ok: true };
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @AuthenticatedUser() user: UserPayload,
+  ) {
+    const { title, content } = createQuestionSchema.parse(body);
 
-    // const { email, name, password } = CreateQuestionSchema.parse(body);
+    const slug = title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-');
 
-    // return this.prisma.question.create({
-    //   data: {
-    //     ...body,
-    //   },
-    // });
+    return this.prisma.question.create({
+      data: {
+        slug,
+        title,
+        content,
+        authorId: user.sub,
+      },
+    });
   }
 }
